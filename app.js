e983b205a76cc4c377110c834251f786a07e2524
MOHIT-KUMAR-PATIDAR <76639143+MOHIT-KUMAR-PATIDAR@users.noreply.github.com>
2021-12-20 12:09:03 +0530

    Add files via upload
    Link to this website deployed via heroku is 
    
    https://sleepy-woodland-88456.herokuapp.com/

---
+++ app.js
@@ -0,0 +1,81 @@
+//jshint esversion:6
+
+const express=require("express");
+const bodyParser=require("body-parser");
+const request= require("request");
+const https=require("https");
+const { response } = require("express");
+const app=express();
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({extended:true}));
+
+app.get("/",function(req,res){
+    res.sendFile(__dirname+"/signup.html")
+});
+
+app.post("/", function(req,res){
+const firstName=req.body.fName;
+const lastName=req.body.lName;
+const email=req.body.email;
+
+var data={
+    members:[
+        {
+            email_address: email,
+            status:"subscribed",
+            merge_fields:{
+                FNAME:firstName,
+                LNAME:lastName
+
+            }
+        }
+    ]
+};
+
+
+const jsonData= JSON.stringify(data);
+
+const url="https://us1.api.mailchimp.com/3.0/lists/696ea570e3";
+
+const options={
+    method:"POST",
+    auth:"mohit1:43ace290ba0e090374e65d06cd84cf54-us1"
+}
+
+if(response.statusCode === 200){
+    res.sendFile(__dirname +"/success.html");
+} 
+else {
+    res.sendFile(__dirname+"/failure.html");
+}
+
+const request = https.request(url, options, function(response){
+response.on("data",function(data){
+    console.log(JSON.parse(data));
+});
+
+
+});
+
+
+request.write(jsonData);
+request.end();
+
+
+});
+
+
+
+app.listen(process.env.PORT || 3000,function(){
+    console.log("Server Started on port 3000");
+});
+
+
+// f94eccd30817fc5aa3a416f8c92c0b28-us1
+
+// 696ea570e3
+
+// auth:"mohit1:43ace290ba0e090374e65d06cd84cf54-us1"
+
+
+// auth:"angela1:f94eccd30817fc5aa3a416f8c92c0b28-us1"
